14e44c95c7dfc5d4c8f07c809aae3d4f4a03d67d
LuongTrang1706 <luongtrangthitrang0987@gmail.com>
2023-10-04 19:46:04 +0700

    Add project files.

---
+++ Controllers/LearnerController.cs
@@ -0,0 +1,118 @@
+﻿using Lab4.Data;
+using Lab4.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab4.Controllers
+{
+    public class LearnerController : Controller
+    {
+
+        private SchoolContext db;
+        public LearnerController(SchoolContext context)
+        {
+            db = context;
+        }
+        public IActionResult Index()
+        {
+            var learners = db.Learners.Include(m => m.Major).ToList();
+            return View(learners);
+        }
+        public IActionResult Create()
+        {
+            //dùng 1 trong 2 cách để tạo SelectList gửi về View qua ViewBag để
+            //hiển thị danh sách chuyên ngành (Majors)
+            var majors = new List<SelectListItem>(); //cách 1
+            foreach (var item in db.Majors)
+            {
+                majors.Add(new SelectListItem
+                {
+                    Text = item.MajorName,
+                    Value = item.MajorID.ToString()
+                });
+            }
+            ViewBag.MajorID = majors;
+            ViewBag.MajorID = new SelectList(db.Majors, "MajorID", "MajorName"); //cách 2
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("FirstMidName,LastName,MajorID,EnrollmentDate")]
+Learner learner)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Learners.Add(learner);
+                db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            //lại dùng 1 trong 2 cách tạo SelectList gửi về View để hiển thị danh sách Majors
+            ViewBag.MajorID = new SelectList(db.Majors, "MajorID", "MajorName");
+            return View();
+        }
+        //edit
+            public IActionResult Edit(int id)
+            {
+                var learners = db.Learners.Find(id);
+                if (learners == null)
+                {
+                    return NotFound();
+                }
+
+                // Tạo SelectList để hiển thị danh sách chuyên ngành (Majors)
+                var majors = new SelectList(db.Majors, "MajorID", "MajorName");
+                ViewBag.MajorID = majors;
+
+                return View(learners);
+            }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, [Bind("LearnerID,FirstMidName,LastName,MajorID,EnrollmentDate")] Learner learners)
+        {
+            if (id != learners.LearnerID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                db.Update(learners);
+                db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            // Tạo SelectList để hiển thị danh sách Majors
+            ViewBag.MajorID = new SelectList(db.Majors, "MajorID", "MajorName");
+            return View(learners);
+        }
+
+        // Delete
+        public IActionResult Delete(int id)
+        {
+            var learners = db.Learners.Find(id);
+            if (learners == null)
+            {
+                return NotFound();
+            }
+            ViewBag.MajorID = new SelectList(db.Majors, "MajorID", "MajorName");
+            return View(learners);
+           
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var learner = db.Learners.Find(id);
+            if (learner == null)
+            {
+                return NotFound();
+            }
+
+            db.Learners.Remove(learner);
+            db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+
+    }
